978e5e1659d594917ec35dc0989d9982dfe9bfd0
Mr. Lee <Mr. Lee>
2016-09-01 20:52:09 +0800

    write image index to file.

--- src/software/SfM/main_GlobalSfM.cpp
+++ src/software/SfM/main_GlobalSfM.cpp
@@ -18,8 +18,7 @@ using namespace openMVG::sfm;
 #include "third_party/stlplus3/filesystemSimplified/file_system.hpp"
 
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   using namespace std;
   std::cout << std::endl
     << "-----------------------------------------------------------\n"
@@ -33,40 +32,38 @@ int main(int argc, char **argv)
     << "------------------------------------------------------------"
     << std::endl;
 
-
   CmdLine cmd;
-
   std::string sSfM_Data_Filename;
   std::string sMatchesDir;
   std::string sOutDir = "";
-  int iRotationAveragingMethod = int (ROTATION_AVERAGING_L2);
-  int iTranslationAveragingMethod = int (TRANSLATION_AVERAGING_SOFTL1);
+  int iRotationAveragingMethod = int(ROTATION_AVERAGING_L2);
+  int iTranslationAveragingMethod = int(TRANSLATION_AVERAGING_SOFTL1);
   std::string sIntrinsic_refinement_options = "ADJUST_ALL";
 
-  cmd.add( make_option('i', sSfM_Data_Filename, "input_file") );
-  cmd.add( make_option('m', sMatchesDir, "matchdir") );
-  cmd.add( make_option('o', sOutDir, "outdir") );
-  cmd.add( make_option('r', iRotationAveragingMethod, "rotationAveraging") );
-  cmd.add( make_option('t', iTranslationAveragingMethod, "translationAveraging") );
-  cmd.add( make_option('f', sIntrinsic_refinement_options, "refineIntrinsics") );
+  cmd.add(make_option('i', sSfM_Data_Filename, "input_file"));
+  cmd.add(make_option('m', sMatchesDir, "matchdir"));
+  cmd.add(make_option('o', sOutDir, "outdir"));
+  cmd.add(make_option('r', iRotationAveragingMethod, "rotationAveraging"));
+  cmd.add(make_option('t', iTranslationAveragingMethod, "translationAveraging"));
+  cmd.add(make_option('f', sIntrinsic_refinement_options, "refineIntrinsics"));
 
   try {
     if (argc == 1) throw std::string("Invalid parameter.");
     cmd.process(argc, argv);
-  } catch(const std::string& s) {
+  } catch (const std::string& s) {
     std::cerr << "Usage: " << argv[0] << '\n'
-    << "[-i|--input_file] path to a SfM_Data scene\n"
-    << "[-m|--matchdir] path to the matches that corresponds to the provided SfM_Data scene\n"
-    << "[-o|--outdir] path where the output data will be stored\n"
-    << "\n[Optional]\n"
-    << "[-r|--rotationAveraging]\n"
+      << "[-i|--input_file] path to a SfM_Data scene\n"
+      << "[-m|--matchdir] path to the matches that corresponds to the provided SfM_Data scene\n"
+      << "[-o|--outdir] path where the output data will be stored\n"
+      << "\n[Optional]\n"
+      << "[-r|--rotationAveraging]\n"
       << "\t 1 -> L1 minimization\n"
       << "\t 2 -> L2 minimization (default)\n"
-    << "[-t|--translationAveraging]:\n"
+      << "[-t|--translationAveraging]:\n"
       << "\t 1 -> L1 minimization\n"
       << "\t 2 -> L2 minimization of sum of squared Chordal distances\n"
       << "\t 3 -> SoftL1 minimization (default)\n"
-    << "[-f|--refineIntrinsics] Intrinsic parameters refinement option\n"
+      << "[-f|--refineIntrinsics] Intrinsic parameters refinement option\n"
       << "\t ADJUST_ALL -> refine all existing parameters (default) \n"
       << "\t NONE -> intrinsic parameters are held as constant\n"
       << "\t ADJUST_FOCAL_LENGTH -> refine only the focal length\n"
@@ -74,19 +71,18 @@ int main(int argc, char **argv)
       << "\t ADJUST_DISTORTION -> refine only the distortion coefficient(s) (if any)\n"
       << "\t -> NOTE: options can be combined thanks to '|'\n"
       << "\t ADJUST_FOCAL_LENGTH|ADJUST_PRINCIPAL_POINT\n"
-      <<      "\t\t-> refine the focal length & the principal point position\n"
+      << "\t\t-> refine the focal length & the principal point position\n"
       << "\t ADJUST_FOCAL_LENGTH|ADJUST_DISTORTION\n"
-      <<      "\t\t-> refine the focal length & the distortion coefficient(s) (if any)\n"
+      << "\t\t-> refine the focal length & the distortion coefficient(s) (if any)\n"
       << "\t ADJUST_PRINCIPAL_POINT|ADJUST_DISTORTION\n"
-      <<      "\t\t-> refine the principal point position & the distortion coefficient(s) (if any)\n"
-    << std::endl;
-
+      << "\t\t-> refine the principal point position & the distortion coefficient(s) (if any)\n"
+      << std::endl;
     std::cerr << s << std::endl;
     return EXIT_FAILURE;
   }
 
   if (iRotationAveragingMethod < ROTATION_AVERAGING_L1 ||
-      iRotationAveragingMethod > ROTATION_AVERAGING_L2 )  {
+    iRotationAveragingMethod > ROTATION_AVERAGING_L2) {
     std::cerr << "\n Rotation averaging method is invalid" << std::endl;
     return EXIT_FAILURE;
   }
@@ -95,16 +91,16 @@ int main(int argc, char **argv)
     cameras::StringTo_Intrinsic_Parameter_Type(sIntrinsic_refinement_options);
 
   if (iTranslationAveragingMethod < TRANSLATION_AVERAGING_L1 ||
-      iTranslationAveragingMethod > TRANSLATION_AVERAGING_SOFTL1 )  {
+    iTranslationAveragingMethod > TRANSLATION_AVERAGING_SOFTL1) {
     std::cerr << "\n Translation averaging method is invalid" << std::endl;
     return EXIT_FAILURE;
   }
 
   // Load input SfM_Data scene
   SfM_Data sfm_data;
-  if (!Load(sfm_data, sSfM_Data_Filename, ESfM_Data(VIEWS|INTRINSICS))) {
+  if (!Load(sfm_data, sSfM_Data_Filename, ESfM_Data(VIEWS | INTRINSICS))) {
     std::cerr << std::endl
-      << "The input SfM_Data file \""<< sSfM_Data_Filename << "\" cannot be read." << std::endl;
+      << "The input SfM_Data file \"" << sSfM_Data_Filename << "\" cannot be read." << std::endl;
     return EXIT_FAILURE;
   }
 
@@ -112,8 +108,7 @@ int main(int argc, char **argv)
   using namespace openMVG::features;
   const std::string sImage_describer = stlplus::create_filespec(sMatchesDir, "image_describer", "json");
   std::unique_ptr<Regions> regions_type = Init_region_type_from_file(sImage_describer);
-  if (!regions_type)
-  {
+  if (!regions_type) {
     std::cerr << "Invalid: "
       << sImage_describer << " regions type file." << std::endl;
     return EXIT_FAILURE;
@@ -129,25 +124,22 @@ int main(int argc, char **argv)
   // Matches reading
   std::shared_ptr<Matches_Provider> matches_provider = std::make_shared<Matches_Provider>();
   if // Try to read the two matches file formats
-  (
-    !(matches_provider->load(sfm_data, stlplus::create_filespec(sMatchesDir, "matches.e.txt")) ||
-      matches_provider->load(sfm_data, stlplus::create_filespec(sMatchesDir, "matches.e.bin")))
-  )
-  {
+    (
+      !(matches_provider->load(sfm_data, stlplus::create_filespec(sMatchesDir, "matches.e.txt")) ||
+        matches_provider->load(sfm_data, stlplus::create_filespec(sMatchesDir, "matches.e.bin")))
+      ) {
     std::cerr << std::endl
       << "Invalid matches file." << std::endl;
     return EXIT_FAILURE;
   }
 
-  if (sOutDir.empty())  {
+  if (sOutDir.empty()) {
     std::cerr << "\nIt is an invalid output directory" << std::endl;
     return EXIT_FAILURE;
   }
 
-  if (!stlplus::folder_exists(sOutDir))
-  {
-    if (!stlplus::folder_create(sOutDir))
-    {
+  if (!stlplus::folder_exists(sOutDir)) {
+    if (!stlplus::folder_create(sOutDir)) {
       std::cerr << "\nCannot create the output directory" << std::endl;
     }
   }
@@ -175,13 +167,25 @@ int main(int argc, char **argv)
   sfmEngine.SetTranslationAveragingMethod(
     ETranslationAveragingMethod(iTranslationAveragingMethod));
 
-  if (sfmEngine.Process())
-  {
-    std::cout << std::endl << " Total Ac-Global-Sfm took (s): " << timer.elapsed() << std::endl;
+  if (sfmEngine.Process()) {
+    std::cout << std::endl << " Total Ac-Global-Sfm took (s): "
+      << timer.elapsed() << std::endl;
 
     std::cout << "...Generating SfM_Report.html" << std::endl;
     Generate_SfM_Report(sfmEngine.Get_SfM_Data(),
       stlplus::create_filespec(sOutDir, "SfMReconstruction_Report.html"));
+
+    //将索引号保存至文件中
+    SfM_Data my_sfm_data = sfmEngine.Get_SfM_Data();
+    string txtPath = my_sfm_data.s_root_path + "/../" + "txtFiles";
+    mkdir(txtPath.c_str());
+    fstream index(txtPath + "/validImgIndex.txt", ios::out);
+    //存储有效poses的索引号
+    for (Poses::iterator itr = my_sfm_data.poses.begin();
+      itr != my_sfm_data.poses.end(); itr++) {
+      index << itr->first << endl;
+    }
+    index.close();
 
     //-- Export to disk computed scene (data & visualizable results)
     std::cout << "...Export SfM_Data to disk." << std::endl;
